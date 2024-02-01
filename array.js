6a52c86568fe1d2b2652676800a5bb4ed36a2b28
Joy <joydas21891920@gmail.com>
2024-02-01 13:07:42 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,127 @@
+
+// var friednsAge = [11, 21, 23, 24];
+// var picnicFee = [1000, 2000, 10000];
+// var waifu = ["Nami", "Robin", "Boa", "Zoro"];
+// console.log(friednsAge.length);
+// console.log(picnicFee);
+// console.log(waifu.indexOf("Zoro"));
+
+// INDEX
+
+// var numbers = [10, 20, 30, 40, 50];
+// console.log(numbers.indexOf(30));
+
+
+//PUSH-POP
+// var numbers = [10, 20, 30, 40, 50];
+// numbers.unshift(100);
+// console.log(numbers);
+// var element = numbers.shift();
+// console.log(numbers);
+// console.log(element);
+
+
+// COMPARISON OPERATOR
+
+//  var isGraduated = true;
+//  var salary = 20000;
+
+//  if (isGraduated === true && salary === 50000) {
+//     console.log("Esho Biye Kore Feli");
+//  }
+//  else {
+//     console.log("I am sorry Babu");
+//  }
+
+
+// let money = 50;
+// let danishPrise = 40;
+// let butterBunPrice = 25;
+// let biscuitPrice = 20;
+
+// if (danishPrise <= money) {
+//     console.log("Danish Khamu");
+// }
+// else if (butterBunPrice <= money){
+//     console.log("Butter Bun Khamu");
+// }
+// else if (biscuitPrice <= money) {
+//     console.log("Biscuit Khamu");
+// }
+// else {
+//     console.log("Baal O Khaite Parmu Na");
+// }
+
+// var fruits = ["Apple", "Banana", "Orange"];
+
+// var index = fruits.indexOf("Banana");
+// console.log(index);
+// fruits[1] = "Mango";
+// console.log(fruits);
+
+// console.log(fruits);
+// var index = fruits.indexOf("Banana");
+// console.log(index);
+// var newFruits = fruits.splice(1, 1, "Mango");
+// console.log(fruits);
+
+
+
+
+// console.log(fruits);
+// fruits.pop();
+// console.log(fruits);
+// fruits.push("Watermelon");
+// console.log(fruits);
+
+// let myNumber = 59;
+// let tomNumber = 66;
+// let janeNumber = 95;
+// let patelNumbe = 56;
+// let johnNumber = 40;
+
+// if (myNumber >= 80) {
+//     console.log("A");
+// }
+// else if (myNumber < 80 && myNumber >= 60) {
+//     console.log("B");
+// }
+// else if (myNumber < 60 && myNumber >= 50) {
+//     console.log("C");
+// }
+// else if (myNumber < 50 && myNumber >= 40) {
+//     console.log("D");
+// }
+// else{
+//     console.log("Fail");
+// }
+
+
+// let num1 = 13;
+// let num2 = 79;
+// let num3 = 45;
+
+// if (num1 > num2 && num1 > num3) {
+//     console.log("Number 1 is bigger");
+// }
+// else if (num2 > num1 && num2 > num3) {
+//     console.log("Number 2 is bigger");
+// }
+// else {
+//     console.log("Number 3 is bigger");
+// }
+
+
+// let side1 = 9;
+// let side2 = 8;
+// let side3 = 9;
+
+// if (side1 === side2 && side1 === side3) {
+//     console.log("Not a Isoscele");
+// }
+// else if (side1 === side2 || side2 === side3 || side3 === side1) {
+//     console.log("Isosceles");
+// }
+// else{
+//     console.log("Not a Isoscele");
+// }
